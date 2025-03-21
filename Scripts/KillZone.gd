db6709fe42e572f5b5c4cd8d8e973467a470c7e6
AstroBytee <ryan.lac5@icloud.com>
2025-03-21 14:22:05 -0400

    Kill Zone added

---
+++ Scripts/KillZone.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
+	var tween = get_tree().create_tween()
+	tween.tween_property(Engine, "time_scale", 0.05, 0.25).set_trans(Tween.TRANS_EXPO)
+	
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
